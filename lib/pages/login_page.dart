8cb08ff7106987623f3b47ca1307b3a1813afb8d
Yasir Afzal <sheikhyasir621@gmail.com>
2021-07-16 00:24:33 +0500

    Day 4 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,17 +1,65 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+//  const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "login page",
-          style: TextStyle(
-              fontSize: 20, color: Colors.purple, fontWeight: FontWeight.bold),
-        ),
+      color: Colors.white,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 60,
+          ),
+          Image.asset(
+            "assets/images/login.png",
+            fit: BoxFit.cover,
+            height: 150,
+            width: 150,
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Text(
+            "Welcome",
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Enter your username", labelText: "Username"),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: "Enter Password",
+                    hintText: "Enter your password",
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  child: Text("LogIn"),
+                  style: TextButton.styleFrom(),
+                  onPressed: () {
+                    print("Hi Yasir");
+                  },
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
