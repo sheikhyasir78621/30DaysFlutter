fa7dff7116ded0de61721c4442992f19978146e3
Yasir Afzal <sheikhyasir621@gmail.com>
2021-08-05 23:35:25 +0500

    day23 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -3,10 +3,6 @@ import 'dart:convert';
 class CatalogModel {
   static List<Item> items = [];
 
-  static final catModel = CatalogModel._internal();
-  CatalogModel._internal();
-  factory CatalogModel() => catModel;
-
   // Get item by id
   Item getById(int id) =>
       items.firstWhere((element) => element.id == id, orElse: null);
